569309eeff1709202139a07c541a5b85a33d1ab5
Pushpajekumar <mk6358522@gmail.com>
2024-01-29 14:58:48 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
 
 export default function App() {
+  LocalAuthentication.authenticateAsync({
+    promptMessage: "Authenticate to proceed",
+    cancelLabel: "Cancel",
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Pushpaje Kumar!</Text>
       <StatusBar style="auto" />
     </View>
   );
+  Loca;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
